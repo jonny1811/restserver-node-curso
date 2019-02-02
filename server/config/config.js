de9ef71e849894b2fc70a293832486509c6b1f84
Jonny <jonnyda1811@gmail.com>
2019-02-02 11:10:37 -0300

    Restserver - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,23 @@
-
+//////////////////
 // Puerto
+//////////////////
 process.env.PORT = process.env.PORT || 3000;
+
+//////////////////
+// Entorno
+//////////////////
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//////////////////
+// DB
+//////////////////
+let urlDB
+
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+
+}else {
+    urlDB = 'mongodb://adm-user:Jonny123456@ds119765.mlab.com:19765/cafe';
+}
+process.env.URLDB = urlDB;
